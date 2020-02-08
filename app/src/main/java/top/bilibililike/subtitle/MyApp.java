6be22226cc4f2e1d6312bb3497b2482fafbd07fb
Xbs <747368836@qq.com>
2020-02-08 19:42:16 +0800

    save temperary

--- app/src/main/java/top/bilibililike/subtitle/MyApp.java
+++ app/src/main/java/top/bilibililike/subtitle/MyApp.java
@@ -3,6 +3,8 @@ package top.bilibililike.subtitle;
 import android.app.Application;
 import android.content.Context;
 
+import com.tencent.bugly.Bugly;
+
 public class MyApp extends Application {
     public static Context mContext;
 
@@ -10,6 +12,7 @@ public class MyApp extends Application {
     public void onCreate() {
         super.onCreate();
         mContext = getApplicationContext();
+        Bugly.init(mContext,"f040e49a29",true);
     }
 
     public static Context getContext(){
