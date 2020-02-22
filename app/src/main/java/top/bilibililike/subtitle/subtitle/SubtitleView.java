8616ef28e48836b4dabd5bddd69dce43ec3492a7
Xbs <747368836@qq.com>
2020-02-22 16:31:48 +0800

    修改字幕样式

--- app/src/main/java/top/bilibililike/subtitle/subtitle/SubtitleView.java
+++ app/src/main/java/top/bilibililike/subtitle/subtitle/SubtitleView.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.RectF;
-import android.os.Looper;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -17,9 +16,7 @@ import java.util.concurrent.TimeUnit;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
-import top.bilibililike.subtitle.utils.ToastUtil;
 import top.bilibililike.subtitle.utils.Utils;
 
 
@@ -27,8 +24,8 @@ import top.bilibililike.subtitle.utils.Utils;
  * @author Xbs
  * @date 2020年1月15日19:15:14
  */
-public class SubTitleView extends View {
-    private static final String TAG = SubTitleView.class.getSimpleName();
+public class SubtitleView extends View {
+    private static final String TAG = SubtitleView.class.getSimpleName();
     final private TextPaint firstTextPaint;
     final private TextPaint secondTextPaint;
     private long timeFlag;
@@ -36,57 +33,62 @@ public class SubTitleView extends View {
     final private StringBuilder newSubTitleStr;
     final private StringBuilder oldSubTitleStr;
 
-    private int sumWidth ;
+    private int sumWidth;
     private int sumHeight;
 
-    final private int firstTextSize = 24;
-    final private int secondTextSize = 18;
+    final private float firstTextSize = Utils.sp2px(24);
+    final private float secondTextSize = Utils.sp2px(18);
 
     final private float alpha = 0.7f;
 
     final private int textMarging = 5;
 
     final RectF rectF;
+    final RectF firstRectF;
+    final RectF secondRectF;
 
-    Disposable disposable;
+    private Disposable disposable;
 
-    public SubTitleView(Context context, AttributeSet attrs) {
-        super(context,attrs);
+    public SubtitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
         firstTextPaint = new TextPaint();
         firstTextPaint.setColor(Color.WHITE);
         firstTextPaint.setAntiAlias(true);
-        firstTextPaint.setTextSize(Utils.sp2px(firstTextSize));
+        firstTextPaint.setTextSize(firstTextSize);
         firstTextPaint.setTextAlign(Paint.Align.CENTER);
         secondTextPaint = new TextPaint();
         secondTextPaint.setColor(Color.WHITE);
         secondTextPaint.setAntiAlias(true);
-        secondTextPaint.setTextSize(Utils.sp2px(secondTextSize));
+        secondTextPaint.setTextSize(secondTextSize);
         secondTextPaint.setTextAlign(Paint.Align.CENTER);
         backgroundPaint = new Paint();
         backgroundPaint.setColor(Color.parseColor("#1F0F0F"));
-        backgroundPaint.setAlpha((int) (alpha*255));
+        backgroundPaint.setAlpha((int) (alpha * 255));
         newSubTitleStr = new StringBuilder();
         oldSubTitleStr = new StringBuilder();
         rectF = new RectF();
+        firstRectF = new RectF();
+        secondRectF = new RectF();
 
+        newSubTitleStr.append(" 第一行文字要那么长一点才行吗，这会儿");
+        oldSubTitleStr.append(" 第二行文字要那么长一点才行吗，这会儿");
 
 
     }
 
-    private void initTimeTask(){
+    private void initTimeTask() {
         timeFlag = System.currentTimeMillis();
         disposable = Observable
-                .interval(1000,TimeUnit.MILLISECONDS)
+                .interval(1000, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
                 .doOnSubscribe(s -> {
-                    Log.d(TAG,"onSubscribe");
+                    Log.d(TAG, "onSubscribe");
                 })
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnNext(aLong -> {
-                    Log.d(TAG,"onNext timeFlag = " + timeFlag);
-                    if (System.currentTimeMillis() - timeFlag > 15000){
-                        setVisibility(View.INVISIBLE);
-                        ToastUtil.show("暂无同传消息，字幕自动隐藏");
+                    Log.d(TAG, "onNext timeFlag = " + timeFlag);
+                    if (System.currentTimeMillis() - timeFlag > 20000) {
+                        //setVisibility(View.INVISIBLE);
                         disposable.dispose();
                     }
                 })
@@ -95,27 +97,50 @@ public class SubTitleView extends View {
     }
 
 
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        if (newSubTitleStr.length() == 0 && oldSubTitleStr.length() == 0) {
+            setVisibility(View.INVISIBLE);
+            return;
+        }
+
         //清空canvas
         canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
-        final float subWidth = (float) ((sumWidth - Math.pow(newSubTitleStr.length() * Utils.sp2px(firstTextSize),0.5) )/2);
-        final float sub2Width = (float) ((sumWidth - Math.pow(newSubTitleStr.length() * Utils.sp2px(secondTextSize),0.5) )/2);
-        canvas.drawRect(rectF,backgroundPaint);
-        //画字幕
-        canvas.drawText(newSubTitleStr.toString(),subWidth,Utils.sp2px(firstTextSize),firstTextPaint);
-        canvas.drawText(oldSubTitleStr.toString(),sub2Width,Utils.sp2px(firstTextSize+secondTextSize+textMarging),secondTextPaint);
+        //矩形宽度计算
+        final float firstRectWidth = newSubTitleStr.length() * firstTextSize;
+        final float secondRectWidth = oldSubTitleStr.length() * secondTextSize;
+        //字体起始x坐标计算
+        final float firstTextWidth = (float) ((sumWidth - Math.pow(firstRectWidth, 0.5)) / 2);
+        final float secondTextWidth = (float) ((sumWidth - Math.pow(secondRectWidth, 0.5)) / 2);
+
+        //top默认为0
+        firstRectF.left = (rectF.right - firstRectWidth) / 2 - textMarging * 4;
+        firstRectF.right = firstRectF.left + firstRectWidth + textMarging * 4;
+        firstRectF.bottom = firstTextSize + textMarging * 2;
+        canvas.drawRect(firstRectF, backgroundPaint);
+
+        secondRectF.top = firstRectF.bottom;
+        secondRectF.left = (rectF.right - secondRectWidth) / 2 - textMarging * 4;
+        secondRectF.right = secondRectF.left + secondRectWidth + textMarging * 4;
+        secondRectF.bottom = secondRectF.top + secondTextSize + textMarging * 2;
+        canvas.drawRect(secondRectF, backgroundPaint);
+
+        //画文字
+        canvas.drawText(newSubTitleStr.toString(), firstTextWidth, firstTextSize - textMarging, firstTextPaint);
+        canvas.drawText(oldSubTitleStr.toString(), secondTextWidth, firstTextSize + secondTextSize + textMarging, secondTextPaint);
     }
 
 
     @Override
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
-        rectF.bottom = bottom;
+        rectF.bottom = bottom / 2;
         rectF.left = left;
         rectF.right = right;
-        rectF.left = left;
+        rectF.top = top;
+        Log.d(TAG, "bottom = " + bottom + "\ntop = " + top + "\nleft = " + left + "\nright = " + right);
+        firstRectF.right = right;
+        secondRectF.right = right;
         super.onLayout(changed, left, top, right, bottom);
     }
 
@@ -125,30 +150,33 @@ public class SubTitleView extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         sumWidth = MeasureSpec.getSize(widthMeasureSpec);
         sumHeight = MeasureSpec.getSize(heightMeasureSpec);
-        setMeasuredDimension(sumWidth,sumHeight);
+        setMeasuredDimension(sumWidth, sumHeight);
     }
 
-    public void addSubtitle(String subTitleStr){
+    public void addSubtitle(String subTitleStr) {
         timeFlag = System.currentTimeMillis();
         setVisibility(View.VISIBLE);
-        if (disposable == null || disposable.isDisposed()){
+        if (disposable == null || disposable.isDisposed()) {
             disposable = null;
             initTimeTask();
         }
-        if (oldSubTitleStr.length() != 0){
+        if (oldSubTitleStr.length() != 0) {
             oldSubTitleStr.setLength(0);
         }
+        oldSubTitleStr.append(" ");
         oldSubTitleStr.append(newSubTitleStr);
-        if (newSubTitleStr.length() != 0){
+        if (newSubTitleStr.length() != 0) {
             newSubTitleStr.setLength(0);
         }
+        newSubTitleStr.append(" ");
         newSubTitleStr.append(subTitleStr);
+
         invalidate();
     }
 
     @Override
     protected void onDetachedFromWindow() {
-        if (!disposable.isDisposed()){
+        if (!disposable.isDisposed()) {
             disposable.dispose();
             disposable = null;
         }
@@ -157,9 +185,9 @@ public class SubTitleView extends View {
 
     @Override
     protected void onAttachedToWindow() {
-        if (disposable == null || disposable.isDisposed()){
+        if (disposable == null || disposable.isDisposed()) {
             disposable = null;
-           initTimeTask();
+            initTimeTask();
         }
         super.onAttachedToWindow();
     }
